a4b02787456b0482bb704f80401a78cf6841347b
Sarah-Maj <sarahmalshalhoub@gmail.com>
2024-11-30 23:49:17 +0300

    Updated CSS styles for modern, cosy theme with responsive layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Header from './Header';
 import Nav from './Nav';
 import Main from './Main';
 import Footer from './Footer';
+import './Header.css';
+import './Main.css';
+import './Button.css';
+import './Card.css';
+import './App.css';
+import './index.css';
 
 
 
